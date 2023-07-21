e9ba9f83c451376ab8ef63ee3655094bbb6992dc
Minjung Kang <135298407+minjung00@users.noreply.github.com>
2023-07-21 11:33:59 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,11 +1,13 @@
 def solution(s):
+    # 문자열 초기화
     answer = ''
+    # 공백 기준으로 단어별로 분리
     new_list = s.split(' ')
     for i in new_list:
         for j in range(len(i)):
-            if j % 2 == 0:
+            if j % 2 == 0: # 짝수인 경우
                 answer += i[j].upper()
-            else:
+            else: # 홀수인 경우
                 answer += i[j].lower()
-        answer+= ' '
-    return answer[0:-1]
+        answer+= ' ' # 해당 단어 끝에 공백을 추가
+    return answer[0:-1] # 최종 결과 문자열에서 마지막 공백을 제거한 뒤 반환
